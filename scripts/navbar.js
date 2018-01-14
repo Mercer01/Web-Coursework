7ee8a2f1efbab313e1ed2cdc5ab37e10c97892ea
Sam Mercer <sam@mercerfamily.org.uk>
2018-01-14 14:29:35 +0000

    Updates
    it looks pretty

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,10 +3,18 @@ function create_navbar() {
     d1.insertAdjacentHTML('afterbegin',`
     <div class="sidenav">
       <a href="index.html">Home</a>
-      <a href="litreview.html">Litreview</a>
+      <a href="litreview.html">Literature Review</a>
       <a href="worklog.html">Worklogs</a>
-      <a href="video.html">Video</a> 
-    </div>`
+      <a href="video.html">Video</a>
+      <p class="footer_sidebar">
+        <a href="https://goo.gl/v4Wr81">
+            <img id=w3c
+                src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
+                alt="Valid CSS!" />
+          </a>
+        </p>
+    </div>
+    `
   );
 }
 
