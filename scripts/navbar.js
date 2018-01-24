b1607ffa4092e3cc668ee8ce84902e9c7ada7ceb
Sam Mercer <sam@mercerfamily.org.uk>
2018-01-24 14:48:39 +0000

    Auto stash before merge of "master" and "origin/master"

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -7,13 +7,13 @@ function create_navbar() {
       <a href="litreview.html">Literature Review</a>
       <a href="worklog.html">Worklogs</a>
       <a href="video.html">Video</a>
-      <p class="footer_sidebar">
+      <footer class="footer_sidebar">
         <a href="https://goo.gl/v4Wr81">
             <img id=w3c
                 src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
                 alt="Valid CSS!" />
-          </a>
-        </p>
+        </a>
+      </footer>
     </div>
     `
   );
