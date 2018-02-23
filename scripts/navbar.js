51b2a45ed47abc544b899890fd8b3f36226b01a8
Sam Mercer <sam@mercerfamily.org.uk>
2018-02-23 17:50:14 +0000

    Fixing of marking scheme

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -13,6 +13,12 @@ function create_navbar() {
                 src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
                 alt="Valid CSS!" />
         </a>
+          <a href="https://validator.w3.org/check?uri=referer">
+              <img style="border:0;width:88px;height:31px"
+                    src="https://www.w3.org/Icons/valid-html401-blue"
+                    alt="Valid HTML!" />
+        </a>
+</footer>
       </footer>
     </div>
     `
