7a2e532b4c4d4175295ea4b1bc141c2e512951ea
Sam Mercer <sam@mercerfamily.org.uk>
2018-03-02 13:29:55 +0000

    added images

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -6,18 +6,20 @@ function create_navbar() {
       <a href="index.html">Home</a>
       <a href="litreview.html">Literature Review</a>
       <a href="worklog.html">Worklogs</a>
+      <a href="image.html">Images</a>
       <a href="video.html">Video</a>
+
       <footer class="footer_sidebar">
-        <a href="https://goo.gl/v4Wr81">
-            <img id=w3c
-                src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
-                alt="Valid CSS!" />
-        </a>
-          <a href="https://validator.w3.org/check?uri=referer">
-              <img style="border:0;width:88px;height:31px"
-                    src="https://www.w3.org/Icons/valid-html401-blue"
-                    alt="Valid HTML!" />
-        </a>
+      <a href="https://jigsaw.w3.org/css-validator/check/referer">
+      <img style="border:0;width:88px;height:31px"
+          src="https://jigsaw.w3.org/css-validator/images/vcss"
+          alt="Valid CSS!" />
+      </a>
+      <a href="https://validator.w3.org/check?uri=referer">
+          <img style="border:0;width:88px;height:31px"
+                src="https://www.w3.org/Icons/valid-html401-blue"
+                alt="Valid HTML!" />
+      </a>
       </footer>
     </nav>
     `
